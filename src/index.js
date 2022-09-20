efdc78525286279a6839df477037142b7c895ce6
ajkarczewski <karczewskiamanda@gmail.com>
2022-09-20 16:57:15 -0500

    chore: add contact and services pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About"
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="services" element={<Services />} />
      </Route>
     </Routes>
   </BrowserRouter>
