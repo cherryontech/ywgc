3d847b6db6e516015f73137114eddeab1e0b5e26
ajkarczewski <karczewskiamanda@gmail.com>
2022-11-08 20:14:44 -0600

    fix linting error

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Home () {
-    return (
-        <h1 className="text-4xl text-center">This is the About page  👋</h1>
-    )
+  return (
+    <h1 className="text-4xl text-center">This is the About page  👋</h1>
+  )
 }
 
 export default Home
