05f7c1e19a44c20d2e3af68aca8abd83c4098e22
Dreamy <dreamy26@icloud.com>
2022-11-23 17:23:11 -0500

    build: adds margins to:About, Contact, Home

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Home () {
+function Home() {
   return (
-    <h1 className="text-4xl text-center">This is the About page  👋</h1>
+    <div className="pt-16 md:py-8">
+      <div className="pl-24">
+        <div className="pr-24">
+          <h1 className="text-4xl text-center">This is the About page  👋</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Contact () {
+function Contact() {
   return (
-    <h1 className="text-4xl text-center">This is the Contact page 👋</h1>
+    <div className="pt-16 md:py-8">
+      <div className="pl-24">
+        <div className="pr-24">
+          <h1 className="text-4xl text-center">This is the Contact page 👋</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
