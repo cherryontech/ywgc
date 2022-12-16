c5641af47c59b4803c41271fdea57fe7d201572b
Dreamy <dreamy26@icloud.com>
2022-12-16 00:04:58 -0500

    build: Adds place holder text for Services, includes padding

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 function Services () {
   return (
-    <h1 className="text-4xl text-center">This is the Services page 👋</h1>
+    <div className="pt-16 md:py-8">
+      <div className="pl-64">
+        <div className="pr-64">
+          <h1 className="text-4xl text-center">Services</h1>
+          <br />
+          {/* <h1 className="text-4xl text-center" style={{ fontFamily: 'Lato' }}>Coming soon!</h1> */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <br />
+
+          <h2 className="text-2xl">Service #1</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <br />
+
+          <h2 className="text-2xl">Service #2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <br />
+
+          <h2 className="text-2xl">Service #3</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <br />
+
+          <h2 className="text-2xl">Service #4</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <br />
+
+          {/* <h2 style={{ fontFamily: 'Inter' }}>Constructing</h2> */}
+        </div>
+      </div>
+    </div>
+
   )
 }
 
