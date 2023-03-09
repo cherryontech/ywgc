1c3f741055d9cc7bb829e77540bf3c65f1f8b07d
ajkarczewski <karczewskiamanda@gmail.com>
2023-03-09 11:39:48 -0600

    YWGC-14 chore: add hero title, intro text, adjust margin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
       <div className="pl-24">
       <div className="pr-24">
       <h1 className="text-3xl sm:text-4xl text-center">Yorkshire Woods Ground Crew</h1>
-{/* // add top margin to p tag rather than h1 */}
+
       <p className="text-base mt-2.5 sm:mt-7">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Augue mauris augue neque gravida in fermentum et sollicitudin. Lacus sed viverra tellus in hac habitasse platea. Molestie a iaculis at erat pellentesque adipiscing. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Hac habitasse platea dictumst vestibulum rhoncus. Cras pulvinar mattis nunc sed blandit libero volutpat. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Lorem ipsum dolor sit amet consectetur adipiscing elit. Nisl purus in mollis nunc sed id semper. Egestas sed tempus urna et. Elit ut aliquam purus sit amet luctus venenatis lectus magna.
         <br></br>
