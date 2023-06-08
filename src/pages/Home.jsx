0c9ca6db150bd249fdf603220036bd811aecb631
NurkaAmre <nurkerey30@gmail.com>
2023-06-08 15:43:59 +0600

    Business title & description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 function Home () {
   return (
-    <div className="py-[8px] px-[32px] md:py-[16px] bg-white bg-opacity-60 rounded-lg " role="region" aria-label="Business Information">
-      <h1 className="text-center font-header text-3xl md:text-4xl mb-[16px]" role="heading">Yorkshire Woods Ground Crew</h1>
-      <p className="text-base font-body" role="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Augue mauris augue neque gravida in fermentum et sollicitudin. Lacus sed viverra tellus in hac habitasse platea. Molestie a iaculis at erat pellentesque adipiscing. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Hac habitasse platea dictumst vestibulum rhoncus. Cras pulvinar mattis nunc sed blandit libero volutpat. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Lorem ipsum dolor sit amet consectetur adipiscing elit. Nisl purus in mollis nunc sed id semper. Egestas sed tempus urna et. Elit ut aliquam purus sit amet luctus venenatis lectus magna.
+    <div className="mt-[86px] mx-[34px] md:mt-[160px] md:mx-[364px] flex flex-col items-center bg-white bg-opacity-60 rounded-lg" role="region" aria-label="Content Section">
+      <div className="pl-[32px] py-[16px]">
+        <div className="pr-[32px]">
+          <h1 className="text-[50px] md:text-[64px] text-center font-header font-bold">Yorkshire Woods Ground Crew</h1>
+          <p className="text-[24px] px-[38px] font-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus.
       </p>
+    </div>
+      </div>
     </div>
 
   )
