cb4a05badeafac53abe111be6dc76d7d4f647f3f
NurkaAmre <nurkerey30@gmail.com>
2023-06-08 13:02:57 +0600

    Apply custom fonts in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
       fontFamily: {
         header: ['Fjalla One', 'sans-serif'],
-        'sans': ['Merriweather Sans', ...defaultTheme.fontFamily.sans]
+        sans: ['Merriweather Sans', ...defaultTheme.fontFamily.sans]
       }
     },
-    plugins: [],
+    plugins: []
   }
 }
