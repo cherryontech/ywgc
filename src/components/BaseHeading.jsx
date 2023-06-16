43dc6de149738967f014c8dc80b65f15270ee645
NurkaAmre <nurkerey30@gmail.com>
2023-06-16 09:15:39 +0600

    Fix bugs

--- src/components/BaseHeading.jsx
+++ src/components/BaseHeading.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const BaseHeading = ({ level, text }) => {
-  const HeadingTag = `h${level}`;
+  const HeadingTag = `h${level}`
 
   return (
     <HeadingTag className="text-[50px] md:text-[64px] text-center font-header font-bold">
       {text}
     </HeadingTag>
-  );
-};
+  )
+}
 
-export default BaseHeading;
+BaseHeading.propTypes = {
+  level: PropTypes.number,
+  text: PropTypes.string
+}
+
+export default BaseHeading
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import BaseHeading from '../components/BaseHeading'
 
 function Home () {
   return (
     <div className="mt-20 mx-4 md:mt-40 md:mx-64 bg-white bg-opacity-60 rounded-lg" aria-label="Business Information">
       <div className="px-8 flex flex-col justify-center items-center py-4">
+        <BaseHeading level={1} text="Yorkshire Woods Ground Crew" />
         <p className="text-[24px] font-body">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus.
         </p>
