9c4868c6a6ae08c048ecf2217c62e47449637864
jaynaxissa <jaynaxissa@gmail.com>
2023-06-19 21:56:31 +0700

    Add ServiceItem component, removed font in HTML, switched CSS to tailwindCSS,deleted commenting out parts

--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Services from './pages/Services'
 import './index.css'
 import Services from './components/Services'
 
 export default function App () {
   return (
     <div className='wrapper'>
-    <div className='background-image'>
+    <div className="bg-[url('/public/lawncare-bg.jpeg')] bg-no-repeat bg-cover opacity-70">
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='services' element={<Services />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
