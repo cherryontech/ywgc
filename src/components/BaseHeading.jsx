ba5c838d3cff313627ddb784d1ddb2b83efd68e0
Nurgul Kereikhan <nurkerey30@gmail.com>
2023-06-21 07:54:18 +0600

    Business title section (#94)

---
+++ src/components/BaseHeading.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BaseHeading = ({ level, text }) => {
+  const HeadingTag = `h${level}`
+
+  return (
+    <HeadingTag className="text-4xl text-center md:text-6xl">
+      {text}
+    </HeadingTag>
+  )
+}
+
+BaseHeading.propTypes = {
+  level: PropTypes.number,
+  text: PropTypes.string
+}
+
+export default BaseHeading
