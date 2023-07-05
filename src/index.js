55430a9f032b562395dea47a137d39738e982666
NurkaAmre <nurkerey30@gmail.com>
2023-07-05 22:45:44 +0600

    Include the YWGC service area

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Layout from './pages/Layout'
 import Home from './pages/Home'
 import './index.css'
 import Services from './components/Services'
+import ServiceArea from './components/ServiceArea'
 
 export default function App () {
   return (
@@ -16,8 +17,8 @@ export default function App () {
         </Routes>
       </BrowserRouter>
 
-    <Services />
-
+      <Services />
+      <ServiceArea />
   <div className='service-map'></div>
     <div className='flex flex-col p-16 bg-green-700'>
       <h1 className='text-center text-white'>Icons from &quot;Lawn Care&quot; collection, created by Berkah Icon, from <a className='underline' href='https://thenounproject.com/browse/collection-icon/lawn-care-163681/?p=1'>Noun Project</a> CC BY 3.0</h1>
