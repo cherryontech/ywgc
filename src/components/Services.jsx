d6be29fc6b661953f01109964a7e77865659e20f
jaynaxissa <jaynaxissa@gmail.com>
2023-07-12 23:44:41 +0700

    refactor services components

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { serviceData } from '../servicedata'
+import ServiceItem from './ServiceItem'
 
 function Services () {
   return (
-    <div className="flex flex-col w-full px-0 py-[94px] md:px-8 md:py-6 bg-white">
-      <h2 className="mx-auto mb-6 text-6xl text-fjalla">Services</h2>
-      <ul>
-        {serviceData.map((service) => (
-          <li className="flex flex-col items-center mb-8 md:flex-row" key={service.id}>
-            <div className="mx-auto md:mr-8">
-              <img className="max-w-none" src={service.icon} alt={service.title} />
-            </div>
-            <div>
-              <h3 className="mb-2 text-4xl text-center text-fjalla md:text-left">{service.title}</h3>
-              <p className="text-xl text-merriweather">{service.description}</p>
-            </div>
-          </li>
-        ))}
+   <div className="flex flex-col w-full md:py-24 py-6">
+     <h2 className="mx-auto mb-6 text-4xl md:text-6xl text-fjalla">Services</h2>
+    <ul className=''>
+      <li className=''>
+      {serviceData.map((data) => (
+        <ServiceItem key={data.id} data={data} />
+      ))}
+      </li>
       </ul>
-    </div>
+   </div>
   )
 }
 
