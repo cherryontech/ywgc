11abcc976eed9818222bbb3017787c078d4efecd
jaynaxissa <jaynaxissa@gmail.com>
2023-07-13 13:04:07 +0700

    add horizontal line under services title

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,8 @@ import ServiceItem from './ServiceItem'
 function Services () {
   return (
    <div className="flex flex-col w-full md:py-24 py-6">
-     <h2 className="mx-auto mb-6 text-4xl md:text-6xl text-fjalla">Services</h2>
+     <h2 className="mx-auto text-4xl md:text-6xl text-fjalla">Services</h2>
+     <hr className='w-[180px] md:w-[720px] h-px mx-auto mt-2 mb-10 md:mb-2 bg-gray-100 border-0 rounded dark:bg-gray-700'></hr>
     <ul className=''>
       <li className=''>
       {serviceData.map((data) => (
