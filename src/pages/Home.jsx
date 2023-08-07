f6109fec923fd70301119a4bcb5ea7c732a72e43
novellac <38117965+novellac@users.noreply.github.com>
2023-08-06 21:23:07 -0400

    feat: Add contact info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,10 @@ function Home () {
       {/* Mobile Version */}
       <div className='lg:hidden'>
         <div className='flex bg-white py-2.5'>
-          <p className='font-semibold text-lg md:text-xl ml-auto mx-2'>
-            Tap to call!
-          </p>
-          <p className='font-bold text-lg md:text-xl text-green-700 mx-2'>
-            123.456.7890
+          <p className='mx-auto my-8 text-lg font-semibold'>
+            Tap to
+            <a href="mailto:info@yorkshirewoodsgroundcrew.com" className='ml-2 text-xl text-green-700 underline font-header'>email</a>
+            !
           </p>
         </div>
         <div className="p-6 bg-white bg-[url('/public/images/hero.png')] bg-no-repeat bg-cover">
@@ -23,22 +22,22 @@ function Home () {
             </div>
             <div>
               <div className='w-[358px] h-[80px] px-8'>
-                <h2 className='font-bold text-center text-sm uppercase text-zinc-600 leading-5'>
+                <h2 className='text-sm font-bold leading-5 text-center text-zinc-600'>
                   Detroit&apos;s Finest Lawn Care
                 </h2>
               </div>
             </div>
             <div className='gap-16'>
-              <div className='flex flex-col italic text-base leading-normal font-light text-center mb-14 gap-2'>
+              <div className='flex flex-col gap-2 text-xl font-normal text-center mb-14'>
                 <h3 className=''>Personalized Service</h3>
                 <h3 className=''>Jobs Of All Sizes</h3>
                 <h3 className=''>Quality Guaranteed</h3>
               </div>
               <Link
                   to='#services-section'
-                className='text-base md:text-lg text-white font-semibold'
+                  className='text-2xl font-bold text-white'
                 >
-                <div className='flex items-center text-center justify-center md:text-xl bg-green-700 py-3 px-10 rounded-lg'>
+              <div className='flex items-center justify-center px-10 py-3 text-center bg-green-700 rounded-lg'>
                   Our Services
               </div>
               </Link>
@@ -47,7 +46,7 @@ function Home () {
         </div>
       </div>
       {/* web version */}
-      <div className='hidden lg:flex flex-row'>
+      <div className='flex-row hidden lg:flex'>
       <div className='flex flex-row'>
         <img
           className='w-1/3'
@@ -55,29 +54,26 @@ function Home () {
           alt='Freshly mowed vibrant green grass showcasing immaculate grooming. In the foreground, beautiful flower bushes bloom with a variety of white, red, and pink flowers.'
         />
         <div className='w-full'>
-          <div className='flex flex-row absolute right-0 top-0 pt-2'>
-              <p className='font-semibold text-lg md:text-xl mr-4'>
-              Call us today!
-            </p>
-              <p className='font-semibold text-lg md:text-xl text-green-700 mr-4'>
-              123.456.7890
-            </p>
+          <div className='absolute top-0 right-0 flex flex-row pt-2'>
+            <a href="mailto:info@yorkshirewoodsgroundcrew.com" className='mr-4 text-4xl font-normal text-green-700 underline font-header'>
+              Email us today!
+            </a>
           </div>
-          <div className='flex flex-col mx-auto mt-18 py-36 text-center items-center'>
+          <div className='flex flex-col items-center mx-auto text-center mt-18 py-36'>
             <div className='mx-2'>
               <BaseHeading level={1} text='Yorkshire Woods Ground Crew' />
-                <p className='font-bold text-center text-sm uppercase text-zinc-600 leading-5'>
+                <p className='text-sm font-bold leading-5 text-center uppercase text-zinc-600'>
                 Detroit&apos;s Finest Lawn Care
               </p>
             </div>
-              <div className='flex flex-row italic text-base leading-normal font-light lg:text-2xl mb-14 divide-x-2 divide-zinc-500'>
+              <div className='flex flex-row text-base italic font-light leading-normal divide-x-2 lg:text-2xl mb-14 divide-zinc-500'>
               <h3 className='px-2'>Personalized Service</h3>
               <h3 className='px-2'>Jobs Of All Sizes</h3>
               <h3 className='px-2'>Quality Guaranteed</h3>
             </div>
               <Link
                 to='#services-section'
-                className='text-lg md:text-xl text-white font-semibold'
+                className='text-lg font-semibold text-white md:text-xl'
               >
             <div className='flex items-center text-center justify-center bg-green-700 p-2.5 gap-2.5 rounded-lg w-3/5 lg:w-[463px] lg:h-[85px]'>
                 Our Services
