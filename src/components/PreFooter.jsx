64c203f8766184778c7234e1b06ffc5225452f41
Jayna <issariya.yip@mtel.co.th>
2023-08-14 15:17:14 +0700

    added some small details. left out the font as I'm not sure.

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function PreFooter () {
-  return <div className='flex flex-col mx-auto justify-evenly gap-1 w-80 h-52 py-10 px-4 md:flex-row md:justify-between md:w-full md:h-44 md:py-16 md:px-24'>
+  return <div className='flex flex-col mr-auto justify-evenly gap-1 w-80 h-52 py-10 px-4 md:flex-row md:justify-between md:w-full md:h-44 md:py-16 md:px-24'>
     <div className='gap-1.5'>
-      <h3 className='text-lg text-left font-bold md:text-2xl md:font-semibold md:text-right'>Yorkshire Woods Groud Crew</h3>
+      <h3 className='text-lg text-left font-bold  pr-10 md:pr-0 md:text-2xl md:font-semibold md:text-right'>Yorkshire Woods Groud Crew</h3>
       <p className='text-xs text-left font-bold md:text-sm md:font-regular text-stone-600 md:text-left'>Detroit&apos;s Finest Lawn Care</p>
     </div>
     <div className='pt-5 gap-1 md:pt-0'>
