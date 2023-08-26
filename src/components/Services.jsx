aa4d20a023f1c1f8021a3fa2c20210ff5b61a936
NurkaAmre <nurkerey30@gmail.com>
2023-08-26 11:20:59 +0600

    Update btn, email, footer bg

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ function Services () {
   return (
    <div className="flex flex-col w-full md:py-24 py-6">
       <h2 className="mx-auto text-3xl md:text-4xl font-semibold">Services</h2>
-     <hr className='w-[180px] md:w-[720px] h-px mx-auto mt-2 mb-10 md:mb-2 bg-gray-100 border-0 rounded dark:bg-gray-700'></hr>
+      <hr className='w-[180px] md:w-[720px] h-px mx-auto mt-2 mb-10 md:mb-2 bg-gray-100 border-0 rounded dark:bg-custom-color'></hr>
     <ul className=''>
       {serviceData.map((data) => (
         <li className='' key={data.id}>
