e3eac0df24798a36cc879629ee0d7f8095c5361f
Nurgul Kereikhan <nurkerey30@gmail.com>
2023-08-30 11:54:07 +0600

    Update btns (#149)
    * Refactor hero, area weserve, bio section to new layout
    
    * Remove white font color
    
    * Fix linter errors
    
    * Fix fonts for desktop version
    
    * Update btn, email, footer bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-green-700 text-white text-center font-light py-14 text-sm px-6">
+    <div className="bg-custom-color text-white font-light md:py-16 py-10 text-sm px-4 md:px-24">
       <p>Copyright © 2023 Yorkshire Woods Ground Crew. All rights reserved.</p>
-      <div className='py-8'>
+      <div className='py-4'>
         <p>Design: <a className='underline' href="https://www.linkedin.com/in/amanda-karczewski/">Amanda Karczewski</a></p>
         <p>Development: <a className='underline' href="https://github.com/jaynaxissa">Jayna Issariya</a>, <a className='underline' href="https://github.com/NurkaAmre">Nurka Kereikhan</a></p>
         <p>Product Management: <a className='underline' href="https://github.com/alliequintano">Allie Quintano</a></p>
       </div>
-      <p className='text-center text-white'>Icons from &quot;Lawn Care&quot; collection, created by Berkah Icon, from <a className='underline' href='https://thenounproject.com/browse/collection-icon/lawn-care-163681/?p=1'>Noun Project</a> CC BY 3.0</p>
+      <p>Illustrations by Tyler Nickell, @tylrnckll</p>
+      <p className=' text-white'>Icons from &quot;Lawn Care&quot; collection, created by Berkah Icon, from <a className='underline' href='https://thenounproject.com/browse/collection-icon/lawn-care-163681/?p=1'>Noun Project</a> CC BY 3.0</p>
     </div>
   )
 }
